492ce11b5731c63c9d9604efe5e65485ace6c15e
kyung <darong@localhost.localdomain>
2024-02-14 17:14:02 +0900

    파일 모두 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707897527',resources: [
+    cacheName: 'chirpy-1707898407',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
